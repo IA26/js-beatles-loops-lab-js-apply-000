6be8f4fdfd4d47f7eedbbf27e7d251cfbd2215ac
Isaac Avilez <isaac1180@aol.com>
2019-01-05 21:34:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+var musicians = ["John Lennon","Paul McCartney","George Harrison","Ringo Starr"];
+var instruments = ["Guitar","Bass Guitar","Lead Guitar","Drums"];
+function theBeatlesPlay(musicians, instruments) {
+  var Beatles = [];
+  var i;
+  var str;
+  for (i = 0; i <musicians.length; i++) {
+    str = musicians[i] + " plays " + instruments[i];
+    Beatles.push(str);
+  } 
+  return Beatles;
+}
+
+theBeatlesPlay(musicians,instruments);
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + " !!! " ;
+    i++;
+  }
+}
